517b1aa844c407799483a23699605aeaf1cce4c0
Mark Nemec <mrknmc@me.com>
2020-08-24 15:31:38 +0200

    Fix cli tests

--- tests/panoramic/cli/test_cli.py
+++ tests/panoramic/cli/test_cli.py
@@ -1,28 +1,30 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import pytest
-import yaml
 from click.core import Context
 
-from panoramic.cli.cli import ConfigAwareCommand, ContextAwareCommand
-from panoramic.cli.paths import Paths
+from panoramic.cli.cli import (
+    ConfigAwareCommand,
+    ContextAwareCommand,
+    LocalStateAwareCommand,
+)
+from panoramic.cli.errors import ValidationError
 
 
-def test_context_aware_command_no_context(monkeypatch, tmpdir):
+@patch('panoramic.cli.validate.validate_config')
+@patch('panoramic.cli.validate.validate_context', side_effect=ValidationError('test'))
+def test_context_aware_command_invalid(_, __):
     """Check command fails when no context."""
-    monkeypatch.chdir(tmpdir)
     with pytest.raises(SystemExit):
         ContextAwareCommand(name='test-command').invoke(Mock())
 
     # TODO: check that correct exception was raised
 
 
-def test_context_aware_command_context_exists(monkeypatch, tmpdir):
+@patch('panoramic.cli.validate.validate_config')
+@patch('panoramic.cli.validate.validate_context')
+def test_context_aware_command_valid(_, __):
     """Check command succeeds when context exists."""
-    monkeypatch.chdir(tmpdir)
-
-    with Paths.context_file().open('w+') as f:
-        f.write(yaml.dump({'company_slug': 'test-company', 'api_version': 'v1'}))
 
     def test_callback():
         return 10
@@ -33,28 +35,49 @@ def test_context_aware_command_context_exists(monkeypatch, tmpdir):
     assert command.invoke(context) == 10
 
 
-def test_config_aware_command_no_config(monkeypatch, tmpdir):
+@patch('panoramic.cli.validate.validate_config', side_effect=ValidationError('test'))
+def test_config_aware_command_invalid(_):
     """Check command fails when no context."""
-    monkeypatch.setenv('HOME', tmpdir)
-
     with pytest.raises(SystemExit):
         ConfigAwareCommand(name='test-command').invoke(Mock())
 
     # TODO: check that correct exception was raised
 
 
-def test_config_aware_command_config_exists(monkeypatch, tmpdir):
+@patch('panoramic.cli.validate.validate_config')
+def test_config_aware_command_config_exists(_):
     """Check command succeeds when context exists."""
-    monkeypatch.setenv('HOME', tmpdir)
-
-    Paths.config_dir().mkdir()
-    with Paths.config_file().open('w+') as f:
-        f.write(yaml.dump({'client_id': 'test-client-id', 'client_secret': 'test-client-secret', 'api_version': 'v1'}))
 
     def test_callback():
         return 10
 
-    command = ContextAwareCommand(name='test-command', callback=test_callback)
+    command = ConfigAwareCommand(name='test-command', callback=test_callback)
+    context = Context(command)
+
+    assert command.invoke(context) == 10
+
+
+@patch('panoramic.cli.validate.validate_config')
+@patch('panoramic.cli.validate.validate_context')
+@patch('panoramic.cli.validate.validate_local_state', return_value=[ValidationError('test')])
+def test_local_state_aware_command_invalid(_, __, ___):
+    """Check command fails when no context."""
+    with pytest.raises(SystemExit):
+        LocalStateAwareCommand(name='test-command').invoke(Mock())
+
+    # TODO: check that correct exception was raised
+
+
+@patch('panoramic.cli.validate.validate_config')
+@patch('panoramic.cli.validate.validate_context')
+@patch('panoramic.cli.validate.validate_local_state')
+def test_local_state_aware_command_valid(_, __, ___):
+    """Check command succeeds when context exists."""
+
+    def test_callback():
+        return 10
+
+    command = ConfigAwareCommand(name='test-command', callback=test_callback)
     context = Context(command)
 
     assert command.invoke(context) == 10
