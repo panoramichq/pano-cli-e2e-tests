1d69e9ca35b9ca141bbbfb8374ce2fee59b433b7
Mark Nemec <mrknmc@me.com>
2020-08-27 13:47:35 +0200

    Cache client id, secret and company slug

--- src/panoramic/cli/config/auth.py
+++ src/panoramic/cli/config/auth.py
@@ -1,3 +1,4 @@
+import functools
 import os
 
 from panoramic.cli.local.file_utils import read_yaml
@@ -12,6 +13,7 @@ def get_client_secret_env_var() -> str:
     return os.environ['PANO_CLIENT_SECRET']
 
 
+@functools.lru_cache()
 def get_client_id() -> str:
     try:
         return get_client_id_env_var()
@@ -19,6 +21,7 @@ def get_client_id() -> str:
         return read_yaml(Paths.config_file())['client_id']
 
 
+@functools.lru_cache()
 def get_client_secret() -> str:
     try:
         return get_client_secret_env_var()
