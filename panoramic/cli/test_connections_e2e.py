782727973abe8897d4ff6f26ed61f0e87fd98b76
Matej Vetrak <matej.vetrak@panoramichq.com>
2020-11-11 15:29:25 +0100

    Fix command name to connection

--- panoramic/cli/test_connections_e2e.py
+++ panoramic/cli/test_connections_e2e.py
@@ -15,7 +15,7 @@ def test_connections_e2e(monkeypatch, tmpdir):
     result = runner.invoke(
         cli,
         [
-            'connections',
+            'connection',
             'create',
             'my-connection', '--type', 'postgres',
             '--user',
@@ -48,28 +48,28 @@ def test_connections_e2e(monkeypatch, tmpdir):
         }
 
     # List
-    result = runner.invoke(cli, ['connections', 'list'])
+    result = runner.invoke(cli, ['connection', 'list'])
     assert result.exit_code == 0, result.output
     assert result.output == 'my-connection: postgres://my-user:*****@localhost:5432/my_db\n'
 
     # Update
-    result = runner.invoke(cli, ['connections', 'update', 'my-connection', '--database-name', 'my-new-db',
+    result = runner.invoke(cli, ['connection', 'update', 'my-connection', '--database-name', 'my-new-db',
                                  '--no-test'])
     assert result.exit_code == 0, result.output
 
     # List
-    result = runner.invoke(cli, ['connections', 'list'])
+    result = runner.invoke(cli, ['connection', 'list'])
     assert result.exit_code == 0, result.output
     assert result.output == 'my-connection: postgres://my-user:*****@localhost:5432/my-new-db\n'
 
     # Update
-    result = runner.invoke(cli, ['connections', 'remove', 'my-connection'])
+    result = runner.invoke(cli, ['connection', 'remove', 'my-connection'])
     assert result.exit_code == 0, result.output
 
     # List
-    result = runner.invoke(cli, ['connections', 'list'])
+    result = runner.invoke(cli, ['connection', 'list'])
     assert result.exit_code == 0, result.output
-    assert result.stdout.startswith('No connections found.\nUse "pano connections create" to create')
+    assert result.stdout.startswith('No connections found.\nUse "pano connection create" to create')
 
 
 @pytest.mark.vcr
@@ -77,9 +77,9 @@ def test_connections_list_fail_e2e(monkeypatch, tmpdir):
     monkeypatch.setenv('HOME', str(tmpdir))
     runner = CliRunner()
 
-    result = runner.invoke(cli, ['connections', 'list'])
+    result = runner.invoke(cli, ['connection', 'list'])
     assert result.exit_code == 0, result.output
-    assert result.stdout.startswith('No connections found.\nUse "pano connections create" to create')
+    assert result.stdout.startswith('No connections found.\nUse "pano connection create" to create')
 
 
 @pytest.mark.vcr
@@ -87,6 +87,6 @@ def test_connections_update_fail_e2e(monkeypatch, tmpdir):
     monkeypatch.setenv('HOME', str(tmpdir))
     runner = CliRunner()
 
-    result = runner.invoke(cli, ['connections', 'update', 'my-connection'])
+    result = runner.invoke(cli, ['connection', 'update', 'my-connection'])
     assert result.exit_code == 1, result.output
     assert result.stdout.startswith('Error: Connection with name "my-connection" not found.\n')
